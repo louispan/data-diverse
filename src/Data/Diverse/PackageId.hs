d5769d7c715c0934a32af5197c4344169108d000
Louis Pan <louis@lory.local>
2017-06-25 08:20:15 +1000

    updated PackageId

--- src/Data/Diverse/PackageId.hs
+++ src/Data/Diverse/PackageId.hs
@@ -4,4 +4,4 @@ module Data.Diverse.PackageId where
 
 -- | This is used for the Generic D1 metadata.
 -- NB. package id is obtained by running `stack exec ghc-pkg describe data-diverse`
-type PackageId = "data-diverse-0.1.0.0-15JNBdcTY3F9aOKAG5iNge"
+type PackageId = "data-diverse-0.2.0.0-GlnmSQqNwDC9lfFZmoKMcf"
