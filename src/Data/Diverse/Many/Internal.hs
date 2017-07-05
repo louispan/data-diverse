627b272acffb161ea30624552daf66fa764ca3a1
Louis Pan <louis@lory.local>
2017-07-05 21:42:02 +1000

    trying ghc 8.0.2

--- src/Data/Diverse/Many/Internal.hs
+++ src/Data/Diverse/Many/Internal.hs
@@ -15,9 +15,6 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
--- Some constraints are for extra safety and uniqueness so are not actually redundant
-{-# OPTIONS_GHC -Wno-redundant-constraints #-}
-
 module Data.Diverse.Many.Internal (
     -- * 'Many' type
       Many(..) -- Exporting constructor unsafely!
